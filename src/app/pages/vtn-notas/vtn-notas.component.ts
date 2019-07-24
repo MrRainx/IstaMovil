58e5365b30e79e0afe41fe203b9c3cda820b25d9
keke1917 <locmarce17@hotmail.com>
2019-07-24 14:52:51 -0500

    Generamos la ventana para la visualizacion de las notas

---
+++ src/app/pages/vtn-notas/vtn-notas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vtn-notas',
+  templateUrl: './vtn-notas.component.html',
+  styleUrls: ['./vtn-notas.component.scss'],
+})
+export class VtnNotasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
