a9db20df0b95e49c65010850888fbbb89b243d9f
MrRainx <diegocondo10@yahoo.es>
2019-07-25 23:30:54 -0500

    config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: 'periodos/:cedula', loadChildren: './pages/periodos/periodos.module#PeriodosPageModule' },
   { path: 'cursos/:cedula/:idPeriodo', loadChildren: './pages/cursos/cursos.module#CursosPageModule' },
   { path: 'materias/:cedula/:idPeriodo/:cursoNombre', loadChildren: './pages/materias/materias.module#MateriasPageModule' },
-  { path: 'alumnos-curso/:cedula/:idPeriodo/:cursoNombre/:idMateria', loadChildren: './pages/alumnos-curso/alumnos-curso.module#AlumnosCursoPageModule' }
+  { path: 'alumnos-curso/:cedula/:idPeriodo/:cursoNombre/:idMateria', loadChildren: './pages/alumnos-curso/alumnos-curso.module#AlumnosCursoPageModule' },
   { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
   { path: 'card', loadChildren: './pages/card/card.module#CardPageModule' },
   { path: 'informacion', loadChildren: './pages/informacion/informacion.module#InformacionPageModule' }
